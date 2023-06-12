2aaaaeb9258bec28e76f590955cf7b1dfe950147
Namelessbastard <105130950+Namelessbastard@users.noreply.github.com>
2023-06-12 01:57:56 +0100

    Add files via upload

---
+++ display tuto.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+
+pygame.init()
+size = width, height = 1280, 720
+screen = pygame.display.set_mode(size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
